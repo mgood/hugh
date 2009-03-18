40919c9ccce2158e48b997812806d4ef6f88ce95
Matt Good <matt@choicevendor.com>
2009-03-18 15:09:16 -0700

    use find_packages() in setup.py to make sure sub-packages are installed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='Hugh',
     version='0.1',
-    packages=['hugh'],
+    packages=find_packages(),
     install_requires = [
         'Werkzeug',
     ],
